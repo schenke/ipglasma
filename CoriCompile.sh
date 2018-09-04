d4444acfc5ac2676e3fadd9319dbbd1ab483db40
nmueller1988 <nmueller@bnl.gov>
2018-09-04 12:34:30 -0700

    using comiler optimizations also here

--- CoriCompile.sh
+++ CoriCompile.sh
@@ -7,7 +7,7 @@ module load cray-fftw
 rm *exe
 cd src
 rm *exe
-CC -std=c++11 NIKLAS_MAIN.cpp -o GENERATE_IPGLASMA_DIJET.exe Fragmentation.cpp FFT.cpp Matrix.cpp Setup.cpp Init.cpp Random.cpp Group.cpp Lattice.cpp Cell.cpp Glauber.cpp Util.cpp Evolution.cpp GaugeFix.cpp Spinor.cpp MyEigen.cpp -I/usr/local/include -L/usr/local/include -L/usr/local/lib `gsl-config --libs` `gsl-config --cflags`  -lfftw3 -lfftw3_threads -lm -lgsl -lgslcblas -fopenmp
+CC -std=c++11 -O3 NIKLAS_MAIN.cpp -o GENERATE_IPGLASMA_DIJET.exe Fragmentation.cpp FFT.cpp Matrix.cpp Setup.cpp Init.cpp Random.cpp Group.cpp Lattice.cpp Cell.cpp Glauber.cpp Util.cpp Evolution.cpp GaugeFix.cpp Spinor.cpp MyEigen.cpp -I/usr/local/include -L/usr/local/include -L/usr/local/lib `gsl-config --libs` `gsl-config --cflags`  -lfftw3 -lfftw3_threads -lm -lgsl -lgslcblas -fopenmp
 cp GENERATE_IPGLASMA_DIJET.exe ..
 cd ..
 
